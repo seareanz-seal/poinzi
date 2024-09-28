d71ca2a3fc22249640829c3a7bd54c97dd3a5516
sereanz-seal <Affandizaenal5@gmail.com>
2024-09-28 10:48:18 +0700

    wadwa

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,24 +2,20 @@ package com.example;
 
 import javax.swing.JFrame;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    // Membuat jendela utama untuk game
-    JFrame window = new JFrame("Shadow Woods");
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    window.setResizable(false);
-    
-    GamePanel gamePanel = new GamePanel();
-    window.add(gamePanel);
+public class App {
+    public static void main(String[] args) {
+        // Membuat jendela utama untuk game
+        JFrame window = new JFrame("Shadow Woods");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
 
-    window.pack();
-    window.setVisible(true);
-    window.setLocationRelativeTo(null);
-        }
+        window.pack();
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+
+        gamePanel.startGameThread();
+    }
 }
